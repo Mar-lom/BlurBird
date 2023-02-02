46482d96af6c68f1d3bb668ef05dcbbcea9ae4d2
Spencer <Spencerksomething+git@gmail.com>
2023-02-02 12:34:30 -0500

    integrated mediapipe and set up basic hand tracking

--- main.py
+++ main.py
@@ -2,29 +2,56 @@ import cv2 as cv
 import mediapipe as mp
 
 
-videoCap = cv.VideoCapture(0)
+mp_hands = mp.solutions.hands
+mp_draw = mp.solutions.drawing_utils
+mp_drawStyle = mp.solutions.drawing_styles
 
-if not videoCap.isOpened():
-    print("The Camera cant open") #check if the camera can open
 
-while videoCap.isOpened():
-    ret, frame = videoCap.read()
+def main():
+    videoCap = cv.VideoCapture(0)
+    hands = mp_hands.Hands(
+        model_complexity = 0,
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5
+    )#setting up params for the hand tracker
 
-    key = cv.waitKey(10)
-    if key ==27: #esc
-        break
+    if not videoCap.isOpened():
+        print("The Camera cant open") #check if the camera can open
 
-    if ret == True:
-        #print("Reading Video")
-        #Display the Video
-        cv.imshow('Output', frame)
-        if cv.waitKey(25) & 0xFF == ord('x'):
+    while True:
+        ret, frame = videoCap.read()
+        key = cv.waitKey(10)
+
+        if key ==27: #ESC to close window
             break
+        frame.flags.writeable = False #makes it so that the image data is passed by reference before inital processing,
+                                        #improves performance
 
-    else:
-        break
-videoCap.release()
+        frame =cv.cvtColor(frame, cv.COLOR_BGR2RGB)#convert image to RGB for mediapipe processing
+        frame = cv.flip(frame, 1)  # invert the display, so it looks correct left to right
+        tracking = hands.process(frame)#tracking sends the converted image data through the mediapipe .Hands() function
+                                        #to get landmark data
 
-cv.destroyAllWindows()
+        frame.flags.writeable = True
+        frame = cv.cvtColor(frame, cv.COLOR_RGB2BGR)#converts image back to standard output after landmark data obtained
+
+        if tracking.multi_hand_landmarks: # if the frame returns landmark data->
+            for hand_landmarks in tracking.multi_hand_landmarks: #draw each landmark and each connnection
+                mp_draw.draw_landmarks(
+                    frame,
+                    hand_landmarks,
+                    mp_hands.HAND_CONNECTIONS,
+                    mp_drawStyle.get_default_hand_landmarks_style(),
+                    mp_drawStyle.get_default_hand_connections_style()
+
+                )
+        cv.imshow('Output', frame)# output window
 
 
+    videoCap.release()
+
+    cv.destroyAllWindows()
+
+
+if __name__== "__main__":
+    main()
