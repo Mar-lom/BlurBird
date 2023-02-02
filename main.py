7bae597a486a6e820b7511e59c5f7b3565192113
Spencer <Spencerksomething+git@gmail.com>
2023-02-02 11:14:29 -0500

    added alias to cv2 added escape function

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
-import cv2
+import cv2 as cv
 
-videoCap = cv2.VideoCapture(0)
+videoCap = cv.VideoCapture(0)
 
 if not videoCap.isOpened():
     print("The Camera cant open") #check if the camera can open
 
 while videoCap.isOpened():
     ret, frame = videoCap.read()
+
+    key = cv.waitKey(10)
+    if key ==27: #esc
+        break
+
     if ret == True:
         #print("Reading Video")
         #Display the Video
-        cv2.imshow('Output', frame)
-        if cv2.waitKey(25) & 0xFF == ord('x'):
+        cv.imshow('Output', frame)
+        if cv.waitKey(25) & 0xFF == ord('x'):
             break
+
     else:
         break
 videoCap.release()
 
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
 
 
